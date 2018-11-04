1975c10799d67f0b79483b189e1b6644217b0a56
zibengou <zibengou@outlook.com>
2018-11-04 23:36:49 +0800

    update

--- App.js
+++ App.js
@@ -1,88 +1,60 @@
 import React from 'react';
-import { Icon } from 'react-native-elements'
-import { Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import {Icon} from 'react-native-elements'
+import {TabNavigator} from 'react-navigation';
+import HomePage from './src/page/HomePage';
+import HotPage from './src/page/HotPage';
+import AccountPage from './src/page/AccountPage';
 
 export default class App extends React.Component {
-  render() {
-    return <RootTab />;
-  }
-}
-
-class HotScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hot!</Text>
-      </View>
-    );
-  }
-}
-
-class FollowScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Follow!</Text>
-      </View>
-    );
-  }
-}
-
-class AccountScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Account！</Text>
-      </View>
-    );
-  }
-}
-
-const RootTab =  TabNavigator({
-  hot: {
-    screen: HotScreen,
-    path: '/hot',
-    navigationOptions: {
-      tabBarLabel: '热门',
-      tabBarIcon: ({ tintColor, focused }) => (
-        <Icon
-          name='whatshot'
-          size={30}
-          type="MaterialIcons"
-          color={tintColor}
-        />
-      ),
+    render() {
+        return <RootTab/>;
     }
-  },
-  follow: {
-    screen: FollowScreen,
-    path: '/follow',
-    navigationOptions: {
-      tabBarLabel: '关注',
-      tabBarIcon: ({ tintColor, focused }) => (
-        <Icon
-          name='eye'
-          size={30}
-          type="octicon"
-          color={tintColor}
-        />
-      ),
-    }
-  },
-  account: {
-    screen: AccountScreen,
-    path: '/account',
-    navigationOptions: {
-      tabBarLabel: '我的',
-      tabBarIcon: ({ tintColor, focused }) => (
-        <Icon
-          name='user'
-          size={30}
-          type="font-awesome"
-          color={tintColor}
-        />
-      ),
-    }
-  },
+}
+
+const RootTab = TabNavigator({
+    home: {
+        screen: HomePage,
+        path: '/home',
+        navigationOptions: {
+            tabBarLabel: '首页',
+            tabBarIcon: ({tintColor, focused}) => (
+                <Icon
+                    name='home'
+                    size={30}
+                    type="foundation"
+                    color={tintColor}
+                />
+            ),
+        }
+    },
+    hot: {
+        screen: HotPage,
+        path: '/hot',
+        navigationOptions: {
+            tabBarLabel: '热门',
+            tabBarIcon: ({tintColor, focused}) => (
+                <Icon
+                    name='whatshot'
+                    size={30}
+                    type="MaterialIcons"
+                    color={tintColor}
+                />
+            ),
+        }
+    },
+    account: {
+        screen: AccountPage,
+        path: '/account',
+        navigationOptions: {
+            tabBarLabel: '我的',
+            tabBarIcon: ({tintColor, focused}) => (
+                <Icon
+                    name='user'
+                    size={30}
+                    type="font-awesome"
+                    color={tintColor}
+                />
+            ),
+        }
+    },
 });
